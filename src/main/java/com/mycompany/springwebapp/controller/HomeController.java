15ada547c15d5808e414ff8f062ac6f527af9e07
kimfefe <kimcheol8@gmail.com>
2023-07-18 10:30:53 +0900

    수정함

--- src/main/java/com/mycompany/springwebapp/controller/HomeController.java
+++ src/main/java/com/mycompany/springwebapp/controller/HomeController.java
@@ -12,10 +12,7 @@ public class HomeController {
 	@RequestMapping("/")
 	public String index() {
 		log.info("실행1");
-<<<<<<< HEAD
 		log.info("실행2");
-=======
->>>>>>> branch 'master' of https://github.com/kimfefe/springwebapp.git
 		return "index";
 	}
 }
